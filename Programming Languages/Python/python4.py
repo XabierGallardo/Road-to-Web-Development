f87becda29cbd29d9e0eecfe8b3fcb8b238b80b1
XabierGallardo <javiergallard91@gmail.com>
2022-12-10 11:29:59 -0300

    Updated final 2 exercises on lesson 4

--- Programming Languages/Python/python4.py
+++ Programming Languages/Python/python4.py
@@ -197,3 +197,103 @@ class Alumnos:
 # bloque principal
 alumnos=Alumnos()
 alumnos.menu()
+
+
+
+'''
+# Ejercicio 7
+Un banco tiene 3 clientes que pueden hacer depósitos y extracciones. También el banco
+requiere que al final del día calcule la cantidad de dinero que hay depositado.
+'''
+class Cliente:
+
+    def __init__(self,nombre):
+        self.nombre=nombre
+        self.monto=0
+
+    def depositar(self,monto):
+        self.monto=self.monto+monto
+
+    def extraer(self,monto):
+        self.monto=self.monto-monto
+
+    def retornar_monto(self):
+        return self.monto
+
+    def imprimir(self):
+        print("{} tiene depositada la suma de {}".format(self.nombre,self.monto))
+
+
+class Banco:
+
+    def __init__(self):
+        self.cliente1=Cliente("Juan")
+        self.cliente2=Cliente("Ana")
+        self.cliente3=Cliente("Diego")
+
+    def operar(self):
+        self.cliente1.depositar(100)
+        self.cliente2.depositar(150)
+        self.cliente3.depositar(200)
+        self.cliente3.extraer(150)
+
+    def depositos_totales(self):
+        total=self.cliente1.retornar_monto()+self.cliente2.retornar_monto()+self.cliente3.retornar_monto()
+        print("El total de dinero del banco es: {}".format(total))
+        self.cliente1.imprimir()
+        self.cliente2.imprimir()
+        self.cliente3.imprimir()
+
+
+# bloque principal        
+
+banco1=Banco()
+banco1.operar()
+banco1.depositos_totales()
+
+
+
+'''
+Ejercicio 8
+Plantear un programa que permita jugar a los dados. Las reglas de juego son:
+se tiran tres dados si los tres salen con el mismo valor mostrar un mensaje que "gano", sino
+"perdió"
+'''
+import random
+
+class Dado:
+
+    def tirar(self):
+        self.valor=random.randint(1,6)
+
+    def imprimir(self):
+        print("Valor del dado: {}".format(self.valor))
+
+    def retornar_valor(self):
+        return self.valor
+
+
+class JuegoDeDados:
+
+    def __init__(self):
+        self.dado1=Dado()
+        self.dado2=Dado()
+        self.dado3=Dado()
+
+    def jugar(self):
+        self.dado1.tirar()
+        self.dado1.imprimir()
+        self.dado2.tirar()
+        self.dado2.imprimir()
+        self.dado3.tirar()
+        self.dado3.imprimir()
+        if self.dado1.retornar_valor()==self.dado2.retornar_valor() and self.dado1.retornar_valor()==self.dado3.retornar_valor():
+            print("Ganó")
+        else:
+            print("Perdió")
+
+
+# bloque principal del programa
+
+juego_dados=JuegoDeDados()
+juego_dados.jugar()
